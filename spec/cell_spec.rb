1a1386cd0703fa95505720a710a3d330648087b7
Brian Hayes <Brian.hayes1200@gmail.com>
2022-12-13 13:42:36 -0700

    Refactor: refactor all files

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,16 +2,15 @@ require 'rspec'
 require './lib/piece'
 require './lib/cell'
 
-
 RSpec.describe Cell do
-  it "exists" do
+  it 'exists' do
     cell = Cell.new(0, 0)
 
     expect(cell).to be_a(Cell)
   end
 
-  it "has readable attributes" do
-   cell = Cell.new(0, 0)
+  it 'has readable attributes' do
+    cell = Cell.new(0, 0)
 
     expect(cell.row).to eq(0)
     expect(cell.column).to eq(0)
@@ -24,22 +23,22 @@ RSpec.describe Cell do
     expect(cell1.piece).to eq(nil)
   end
 
-  it "will check to see if cell is empty" do
+  it 'will check to see if cell is empty' do
     cell = Cell.new(0, 0)
 
     expect(cell.empty?).to eq(true)
   end
 
-  it "will place a piece on the cell for user" do 
-    cell = Cell.new(0,0) 
+  it 'will place a piece on the cell for user' do
+    cell = Cell.new(0, 0)
     piece = Piece.new(:user)
     cell.place_piece(piece)
-    
-    expect(cell.empty?).to eq(false)
-  end 
 
-  it 'will render the cell' do 
-    cell = Cell.new(0,0) 
+    expect(cell.empty?).to eq(false)
+  end
+
+  it 'will render the cell' do
+    cell = Cell.new(0, 0)
     cell1 = Cell.new(1, 1)
     cell2 = Cell.new(2, 2)
     piece = Piece.new(:user)
@@ -50,16 +49,15 @@ RSpec.describe Cell do
     expect(cell.render).to eq('X')
     expect(cell1.render).to eq('O')
     expect(cell2.render).to eq('.')
-  end 
+  end
 
   it 'will access and manipulate cells in a hash' do
-
-    board = { 
-      cell0: Cell.new(0,0), 
+    board = {
+      cell0: Cell.new(0, 0),
       cell1: Cell.new(1, 2),
       cell2: Cell.new(2, 2)
     }
-   
+
     piece = Piece.new(:user)
     piece2 = Piece.new(:computer)
     board[:cell0].place_piece(piece)
